edf45898a6bf149fbc48353293bc942dce2be1c6
Rasheed khan <rktechnicalplus@gmail.com>
2023-05-22 12:58:48 +0530

    middlewares
    complete code till middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,4 +74,23 @@ const loginController = async (req, res) => {
   }
 };
 
-module.exports = { registerController, loginController };
+//GET CURRENT USER
+const currentUserController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+    return res.status(200).send({
+      success: true,
+      message: "User Fetched Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "unable to get current user",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, loginController, currentUserController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
   registerController,
   loginController,
+  currentUserController,
 } = require("../controllers/authController");
+const authMiddelware = require("../middlewares/authMiddelware");
 
 const router = express.Router();
 
@@ -12,5 +14,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+
+//GET CURRENT USER || GET
+router.get("/current-user", authMiddelware, currentUserController);
 
 module.exports = router;
